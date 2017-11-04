471cab52161389e2b21b0322a4acc92e5adab8b8
Brian Fischer <starfox139@yahoo.com>
2017-11-04 14:31:18 -0400

    characters work

--- Assets/SCRIPTS/Character.cs
+++ Assets/SCRIPTS/Character.cs
@@ -3,27 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour {
+	private enum ControlType { Human, AI }
 	[SerializeField]
+	private ControlType controlType;
+
+
 	private ControlCharacter controller;
-	[SerializeField]
 	private new Rigidbody2D rigidbody;
+	private new BoxCollider2D collider;
 
 
-	public bool grounded { get; }
+	private Vector2 groundCheckPointLeftLocal;
+	private Vector2 groundCheckPointRightLocal;
+	public bool grounded {
+		get {
+			return Physics2D.OverlapPoint (transform.TransformPoint (groundCheckPointLeftLocal)) != null || Physics2D.OverlapPoint (transform.TransformPoint (groundCheckPointRightLocal)) != null;
+		}
+	}
 
 	public float runSpeed = 1f;
 	public float jumpHeight = 4f;
 
 	private float jumpVelocity {
-		get { return Mathf.Sqrt (2f * jumpHeight * Physics2D.gravity.y); }
+		get { return Mathf.Sqrt (2f * jumpHeight * -Physics2D.gravity.y); }
+	}
+
+	private FrameAction actions;
+
+	void Start () {
+		if (controlType == ControlType.Human) {
+			controller = new ControlCharacterHuman ();
+		}
+		else {
+			throw new System.NotImplementedException ();
+		}
+		collider = GetComponent<BoxCollider2D> ();
+		rigidbody = GetComponent<Rigidbody2D> ();
+		groundCheckPointLeftLocal = collider.offset + Vector2.down * collider.size.y * 0.55f + Vector2.left * collider.size.x * 0.25f;
+		groundCheckPointRightLocal = collider.offset + Vector2.down * collider.size.y * 0.55f + Vector2.right * collider.size.x * 0.25f;
 	}
 
 	void Update () {
-		FrameAction actions = controller.GetActions ();
-		Vector2 velocityChange = Vector2.zero;
-		velocityChange.x += actions.moveDirection * runSpeed * Time.deltaTime;
+		actions = controller.GetActions ();
+	}
+
+	void FixedUpdate () {
+		Vector2 velocity = rigidbody.velocity;
+		velocity.x = runSpeed * actions.moveDirection;
 		if (grounded && actions.jump) {
-			velocityChange.y = jumpVelocity;
+			velocity.y = jumpVelocity;
 		}
+		rigidbody.velocity = velocity;
 	}
 }
--- Assets/SCRIPTS/ControlCharacterHuman.cs
+++ Assets/SCRIPTS/ControlCharacterHuman.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControlCharacterHuman : MonoBehaviour, ControlCharacter {
+public class ControlCharacterHuman : ControlCharacter {
 	public FrameAction GetActions () {
 		return new FrameAction (Mathf.RoundToInt (Input.GetAxis ("Horizontal")), Input.GetButtonDown ("Jump"));
 	}
